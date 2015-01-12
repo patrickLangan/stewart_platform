4a75fc9b290cb6a07e91093dbf0fe3c57f047ab3
Patrick Langan <patrick.g.langan@gmail.com>
2015-01-12 00:37:04 +0000

    Updated to use new device tree overlay

--- startup.sh
+++ startup.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+echo stepper-pru > /sys/devices/bone_capemgr.*/slots
 echo i2c1-400hz > /sys/devices/bone_capemgr.*/slots
 echo i2c2-400hz > /sys/devices/bone_capemgr.*/slots
 cat /sys/devices/bone_capemgr.*/slots
