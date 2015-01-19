e9ebe9600b473bc849e34458810f125587732f04
Patrick Langan <patrick.g.langan@gmail.com>
2015-01-19 05:08:47 +0000

    Pru program now can toggle non-pru gpio pins

--- stepper.asm
+++ stepper.asm
@@ -3,6 +3,10 @@
 
 #define CONST_PRUCFG c4
 
+#define GPIO1               0x4804c000 
+#define GPIO_CLEARDATAOUT   0x190
+#define GPIO_SETDATAOUT     0x194
+
 .macro WAIT
 .mparam reg,clicks
 	MOV reg, clicks
@@ -18,12 +22,21 @@ START:
 	CLR r0, r0, 4
 	SBCO r0, CONST_PRUCFG, 4, 4
 
-	//Switch all the PRU outputs on and off as a test
+	//Constants obtained from: https://groups.google.com/forum/?fromgroups=#!topic/beagleboard/35ZXP82EQjA%5B1-25-false%5D
+	MOV r2, 0b11111111111111111111111111111111
+	MOV r3, GPIO1 | GPIO_SETDATAOUT
+	MOV r4, GPIO1 | GPIO_CLEARDATAOUT
+
+	//Switch all the PRU and GPIO outputs on and off as a test
 	LOOP1:
-		MOV r30, 0
-		WAIT r2, 100000000
+		SBBO r2, r3, 0, 4
 		MOV r30, 0b11011111111111
-		WAIT r2, 100000000
+		WAIT r5, 60000000
+
+		SBBO r2, r4, 0, 4
+		MOV r30, 0
+		WAIT r5, 60000000
+
 		JMP LOOP1
 
 HALT
