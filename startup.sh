01fe14480faf93a1f23e02901f4fb4f106c84f53
Patrick Langan <patrick.g.langan@gmail.com>
2015-01-20 19:06:09 +0000

    Added analog input initialization

--- startup.sh
+++ startup.sh
@@ -28,6 +28,7 @@ echo stepper-pru > /sys/devices/bone_capemgr.*/slots
 echo gpio-enable > /sys/devices/bone_capemgr.*/slots
 echo i2c1-400hz > /sys/devices/bone_capemgr.*/slots
 echo i2c2-400hz > /sys/devices/bone_capemgr.*/slots
+echo cape-bone-iio > /sys/devices/bone_capemgr.*/slots
 cat /sys/devices/bone_capemgr.*/slots
 
 sleep 1
