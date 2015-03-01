61a524dfb486f03b602dc4e833a938fcdf101141
Glenn Langan <glenn@langanengineering.org>
2015-03-01 23:41:40 +0000

    Add fixes to the ip_setup.sh script

--- staticIP/ip_setup.sh
+++ staticIP/ip_setup.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 disable_service(){
-	mv /etc/rc2.d/S"$1""$2" /etc/rc2.d/K"$1""$2"
-	mv /etc/rc3.d/S"$1""$2" /etc/rc3.d/K"$1""$2"
-	mv /etc/rc4.d/S"$1""$2" /etc/rc4.d/K"$1""$2"
-	mv /etc/rc5.d/S"$1""$2" /etc/rc5.d/K"$1""$2"
-	update-rc.d $2 defaults
+	mv /etc/rc2.d/S*"$1" /etc/rc2.d/K01"$1"
+	mv /etc/rc3.d/S*"$1" /etc/rc3.d/K01"$1"
+	mv /etc/rc4.d/S*"$1" /etc/rc4.d/K01"$1"
+	mv /etc/rc5.d/S*"$1" /etc/rc5.d/K01"$1"
+	update-rc.d $1 defaults
 }
 
 # make sure the script is given an argument
@@ -14,16 +14,16 @@ if [ $# -lt 1 ]; then
   exit -1
 fi
 
-echo $1 > ./bbb_addr
+mkdir /root/.ip_addr
+echo $1 > /root/.ip_addr/bbb_addr
 
-ln -sr ./bbb_addr /root/.ip_addr/bbb_addr
-ln -sr ./static_ip.sh /usr/local/bin/static_ip
-ln -sr ./static_ip.service /etc/systemd/system/static_ip.service
+ln -s `pwd`/static_ip.sh /usr/local/bin/static_ip
+ln -s `pwd`/static_ip.service /etc/systemd/system/static_ip.service
 
-disable_service 01 apache2
-#disable_service 02 avahi-daemon
-#disable_service 01 udhcpd
-disable_service 01 wicd
+disable_service apache2
+#disable_service avahi-daemon
+#disable_service udhcpd
+disable_service wicd
 
 systemctl enable `pwd`/static_ip.service
 
