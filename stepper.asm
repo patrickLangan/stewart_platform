af1dcec4cd852739c5e6785238fa38c47e1c5d78
Patrick Langan <patrick.g.langan@gmail.com>
2015-03-11 02:38:43 +0000

    Cleaned up and fixed signed->unsigned error

--- stepper.asm
+++ stepper.asm
@@ -96,6 +96,7 @@ START:
 	MOV r1, CTBIR_1
 	SBBO r0, r1, #0x00, 4
 
+
 	//Load GPIO/PRU addresses into RAM
         MOV r0, MOTOR_STEP
 	//Motor STEP
@@ -177,29 +178,6 @@ START:
 	WIPERAM r0, r1, r2
 
 
-/*
-ASDLKF:
-	MOV r1, 0
-LOOPT:
-	QBEQ BUS0c11, r1, 20
-        MOV r2, GPIO2 | GPIO_SETDATAOUT
-	JMP OUTc11
-BUS0c11:
-        MOV r2, GPIO0 | GPIO_SETDATAOUT
-OUTc11:
-        READRAM CONTROL_VALVE, r1, r3, r4
-        SBBO r4, r2, 0, 4
-
-        ADD r1, r1, 4
-
-	WAIT r5, 50000000
-        QBNE LOOPT, r1, 24
-
-	JMP ASDLKF
-*/
-
-
-
 	//This loop goes through rotary encoders channel 1, setting the initial values
 	MOV r0, LAST_ENCODER1
 	MOV r4, LAST_ENCODER2
@@ -226,6 +204,7 @@ DONEGPIOi:
 	WRITERAM r0, r2, r1
 	QBNE INIT2, r0, r4
 
+
 	//MAIN PROGRAM LOOP
 LOOP1:
 	//This loop goes through all 12 motors and steps them asynchronously
@@ -238,29 +217,25 @@ LOOP2:
 	SUB r3, r3, r2
 	SBCO r3, CONST_PRUDRAM, r1, 4 //Motor Time
 
-/*
-	//Check if the motor needs to be stepped
-	READRAM 0, r0, r1, r2
-	READRAM STEP_POSITION, r0, r1, r3
-	QBBS NEGITIVE, r3, 31
-	JMP POSITIVE
-NEGITIVE:
-	MOV r3, 0
-POSITIVE:
-*/
-
         READRAM 0, r0, r1, r2
+
+	//Load encoder position
+	//Set to zero if negitive (valve should never go to a negitive position anyway)
+	//Divide position by 2 (to match the stepper resolution, problems occur otherwise)
         READRAM STEP_POSITION, r0, r1, r3
         QBBC POSPOS, r3, 31
         MOV r3, 0
 POSPOS:
 	LSR r3, r3, 1
 
+	//Load control valve directions, bitshifting to match the control valve to stepper number
 	LSR r8, r0, 3
 	LSL r8, r8, 2
         READRAM CONTROL_DIR, r8, r4, r5
+
+	//Set the goal position to zero if a direction change is required
+	//This results in both motors in a pair to go to zero before the control valve switches
         QBBS NEGSETPOS, r2, 31
-        //IF CONTROL VALVE IS DOWN SET GOAL TO 0
         QBNE POSSETPOS, r5, 0
         MOV r2, 0
         JMP POSSETPOS
@@ -268,7 +243,6 @@ NEGSETPOS:
         //Convert negitive number to positive
         SUB r2, r2, 1
         NOT r2, r2
-        //IF CONTROL VALVE IS UP SET GOAL TO 0
         QBNE POSSETPOS, r5, 1
         MOV r2, 0
 POSSETPOS:
@@ -302,11 +276,12 @@ NXTMOT:
 	ADD r0, r0, 4
 	QBNE LOOP2, r0, 48
 
+
 	//This loop goes through rotary encoders and updates their positions as nesecary
 	MOV r0, 0 //Index
 LOOP3:
-	//Read the current encoder values.
-	//Channel 2 is split up amongst 2 internal bus's, hence the branching.
+	//Read the current encoder values
+	//Channel 2 is split up amongst 2 internal bus's, hence the branching
 	READRAM ROTARY_ENCODER1, r0, r2, r1
 	READGPIO GPIO0, r1, r3, r2
 	READRAM ROTARY_ENCODER2, r0, r3, r1
@@ -320,8 +295,8 @@ DONEGPIO:
 	//Read the stored encoder position
 	READRAM STEP_POSITION, r0, r6, r7
 
-	//Look for rising or falling edges in each channel.
-	//Whether to inc or dec depends the value of the other channel.
+	//Look for rising or falling edges in each channel
+	//Whether to inc or dec depends the value of the other channel
 	READRAM LAST_ENCODER1, r0, r4, r1
 	QBEQ NXTCHAN, r2, r1
 	QBEQ INC, r2, r3
@@ -348,63 +323,63 @@ OUT:
 	ADD r0, r0, 4
 	QBNE LOOP3, r0, 48
 
+
         //This loop changes the directions of the control valves if nessecary
         MOV r0, 0 //Index
         MOV r1, 0 //Index2
 LOOP4:
+	//Load the encoder position of the first motor
+	//Set to zero if negitive (valve should never go to a negitive position anyway)
+	//Divide position by 2 (to match the stepper resolution, problems occur otherwise)
         READRAM STEP_POSITION, r0, r3, r2
+        QBBC POSPOS1, r2, 31
+	MOV r2, 0
+POSPOS1:
+	LSR r2, r2, 1
+
+	//If the first motor's position isn't zero, don't switch the control valve
         ADD r0, r0, 4
+        QBNE NOSWITCH, r2, 0
 
-//*
-        QBBC ASDF, r2, 31
-        SUB r2, r2, 1
-        NOT r2, r2
-ASDF:
-	LSR r2, r2, 1
-//*/
-        QBNE OUT2, r2, 0
-
+	//Read second motor encoder as done for the first motor
         READRAM STEP_POSITION, r0, r3, r2
-
-//*
-        QBBC NOKO, r2, 31
-        SUB r2, r2, 1
-        NOT r2, r2
-NOKO:
+        QBBC POSPOS2, r2, 31
+	MOV r2, 0
+POSPOS2:
 	LSR r2, r2, 1
-//*/
-        QBNE OUT2, r2, 0
 
+	//If the second motor's position isn't zero, don't switch the control valve
+        QBNE NOSWITCH, r2, 0
+
+	//Read set position for the second motor in the pair (should both have the same set position for now)
         READRAM 0, r0, r3, r2
-        QBEQ OUT2, r2, 0
+	//Don't switch the control valve if the set position is zero (no need, valves are closed)
+        QBEQ NOSWITCH, r2, 0
+
+	//Switch the control valve in the cerrect direction
         READRAM CONTROL_DIR, r1, r4, r3
         QBBS NEGATIVE, r2, 31
-        //QBEQ OUT2, r3, 1
         MOV r5, 1
-        SBCO r5, CONST_PRUDRAM, r4, 4 //Control Dir
-	QBEQ BUS0c1, r1, 20
+	QBEQ BUS0NEG, r1, 20
         MOV r2, GPIO2 | GPIO_SETDATAOUT
-	JMP OUTc1
-BUS0c1:
+	JMP STOREDIR
+BUS0NEG:
         MOV r2, GPIO0 | GPIO_SETDATAOUT
-OUTc1:
-        READRAM CONTROL_VALVE, r1, r3, r4
-        SBBO r4, r2, 0, 4
-        JMP OUT2
+        JMP STOREDIR
 NEGATIVE:
-        //QBEQ OUT2, r3, 0
         MOV r5, 0
-        SBCO r5, CONST_PRUDRAM, r4, 4 //Control Dir
-	QBEQ BUS0c2, r1, 20
+	QBEQ BUS0POS, r1, 20
         MOV r2, GPIO2 | GPIO_CLEARDATAOUT
-	JMP OUTc2
-BUS0c2:
+	JMP STOREDIR
+BUS0POS:
         MOV r2, GPIO0 | GPIO_CLEARDATAOUT
-OUTc2:
+
+STOREDIR:
+        SBCO r5, CONST_PRUDRAM, r4, 4 //Control Dir
         READRAM CONTROL_VALVE, r1, r3, r4
         SBBO r4, r2, 0, 4
 
-OUT2:
+NOSWITCH:
         ADD r0, r0, 4
         ADD r1, r1, 4
         QBNE LOOP4, r0, 48
