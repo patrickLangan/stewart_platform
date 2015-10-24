5364af9327a3cb018abf508d549c8b20f7f42286
patrickLangan <patrick.g.langan@gmail.com>
2015-10-24 23:39:53 +0000

    Transfer data between server and client

--- client/control.c
+++ client/control.c
@@ -1,10 +1,14 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 #include <signal.h>
 #include <setjmp.h>
 
 #include <prussdrv.h>
 #include <pruss_intc_mapping.h>
+
+#include <sys/socket.h>
+#include <arpa/inet.h>
 
 #define PRESSURE_SCALE 0.0091558323
 #define LENGTH_SCALE 3.906379093e-4
@@ -18,154 +22,183 @@ static jmp_buf buf;
 
 void signalCatcher (int null)
 {
-	longjmp (buf, 1);
+        longjmp (buf, 1);
 }
 
 int pruInit (void)
 {
-	tpruss_intc_initdata intc = PRUSS_INTC_INITDATA;
+        tpruss_intc_initdata intc = PRUSS_INTC_INITDATA;
 
-	if (prussdrv_init ()) {
-		fprintf (stderr, "prussdrw_init() failed\n");
-		return 1;
-	}
+        if (prussdrv_init ()) {
+                fprintf (stderr, "prussdrw_init() failed\n");
+                return 1;
+        }
 
-	if (prussdrv_open (PRU_EVTOUT_0)) {
-		fprintf (stderr, "prussdrv_open(PRU_EVTOUT_0) failed\n");
-		return 1;
-	}
+        if (prussdrv_open (PRU_EVTOUT_0)) {
+                fprintf (stderr, "prussdrv_open(PRU_EVTOUT_0) failed\n");
+                return 1;
+        }
 
-	if (prussdrv_open (PRU_EVTOUT_1)) {
-		fprintf (stderr, "prussdrv_open(PRU_EVTOUT_1) failed\n");
-		return 1;
-	}
+        if (prussdrv_open (PRU_EVTOUT_1)) {
+                fprintf (stderr, "prussdrv_open(PRU_EVTOUT_1) failed\n");
+                return 1;
+        }
 
-	return 0;
+        return 0;
 }
 
 
 int pruTerminate (void)
 {
-	if (prussdrv_pru_disable (0)) {
-		fprintf (stderr, "prussdrv_pru_disable(0) failed\n");
-		return 1;
-	}
+        if (prussdrv_pru_disable (0)) {
+                fprintf (stderr, "prussdrv_pru_disable(0) failed\n");
+                return 1;
+        }
 
-	if (prussdrv_pru_disable (1)) {
-		fprintf (stderr, "prussdrv_pru_disable(1) failed\n");
-		return 1;
-	}
+        if (prussdrv_pru_disable (1)) {
+                fprintf (stderr, "prussdrv_pru_disable(1) failed\n");
+                return 1;
+        }
 
-	if (prussdrv_exit()) {
-		fprintf (stderr, "prussdrv_exit() failed\n");
-		return 1;
-	}
+        if (prussdrv_exit()) {
+                fprintf (stderr, "prussdrv_exit() failed\n");
+                return 1;
+        }
 
-	return 0;
+        return 0;
 }
 
 int i2cRead (int handle)
 {
-	char buffer[2];
+        char buffer[2];
 
-	i2c_read (handle, buffer, 2);
+        i2c_read (handle, buffer, 2);
 
-	return (int)buffer[0] << 8 | (int)buffer[1];
+        return (int)buffer[0] << 8 | (int)buffer[1];
+}
+
+int inetClientInit (const char *address)
+{
+        int sock;
+        struct sockaddr_in server;
+
+        if ((sock = socket (AF_INET, SOCK_STREAM, 0)) < 0) {
+                fprintf (stderr, "socket(AF_INET, SOCK_STREAM, 0) failed\n");
+                return -1;
+        }
+
+        server.sin_addr.s_addr = inet_addr (address);
+        server.sin_family = AF_INET;
+        server.sin_port = htons (8888);
+
+        if (connect (sock, (struct sockaddr *)&server, sizeof(server)) < 0) {
+                fprintf (stderr, "connect() failed\n");
+                return -1;
+        }
+
+        return sock;
 }
 
 int main (int argc, char **argv)
 {
-	int spiFile1;
-	int spiFile2;
-	int pressHandle1;
-	int pressHandle2;
-	int temp;
-	float force;
-	float length1;
-	float length2;
-	float pressure1;
-	float pressure2;
-	struct timespec waitTime = {0, 100000000};
+        int pressHandle1;
+        int pressHandle2;
 
-	if (setjmp (buf))
-		goto shutdown;
+        int sock;
+        char buffer[255];
 
-	signal (SIGINT, signalCatcher);
+        int temp;
+        float length1;
+        float length2;
+        float pressure1;
+        float pressure2;
 
-	pruInit ();
+        struct timespec waitTime = {0, 100000000};
 
-	if (prussdrv_exec_program (1, "./coldStart.bin")) {
-		fprintf (stderr, "prussdrv_exec_program(1, './coldStart.bin') failed\n");
-		return 1;
-	}
-	puts ("Making sure all valves are closed before starting");
-	sleep (10);
-	puts ("Starting");
+        if (setjmp (buf))
+                goto shutdown;
 
-	prussdrv_map_prumem (PRUSS0_PRU0_DATARAM, &pruDataMem0);
-	pruDataMem0_int = (unsigned int*) pruDataMem0;
+        signal (SIGINT, signalCatcher);
 
-	prussdrv_map_prumem (PRUSS0_PRU1_DATARAM, &pruDataMem1);
-	pruDataMem1_int = (unsigned int*) pruDataMem1;
+        pruInit ();
 
-	if (prussdrv_exec_program (0, "./length.bin")) {
-		fprintf (stderr, "prussdrv_exec_program(0, './length.bin') failed\n");
-		return 1;
-	}
+        if (prussdrv_exec_program (1, "./coldStart.bin")) {
+                fprintf (stderr, "prussdrv_exec_program(1, './coldStart.bin') failed\n");
+                return 1;
+        }
+        puts ("Making sure all valves are closed before starting");
+        sleep (10);
+        puts ("Starting");
 
-	if (prussdrv_exec_program (1, "./stepper.bin")) {
-		fprintf (stderr, "prussdrv_exec_program(1, './stepper.bin') failed\n");
-		return 1;
-	}
+        prussdrv_map_prumem (PRUSS0_PRU0_DATARAM, &pruDataMem0);
+        pruDataMem0_int = (unsigned int*) pruDataMem0;
 
-	pressHandle1 = i2c_open (1, 0x28);
-	pressHandle2 = i2c_open (2, 0x28);
+        prussdrv_map_prumem (PRUSS0_PRU1_DATARAM, &pruDataMem1);
+        pruDataMem1_int = (unsigned int*) pruDataMem1;
 
-	while (1) {
-		temp = pruDataMem0_int[0];
-		if (temp & (1 << 15))
-			temp |= 0xFFFF0000;
-		length1 = (float)temp * LENGTH_SCALE;
+        if (prussdrv_exec_program (0, "./length.bin")) {
+                fprintf (stderr, "prussdrv_exec_program(0, './length.bin') failed\n");
+                return 1;
+        }
 
-		temp = pruDataMem0_int[1];
-		if (temp & (1 << 15))
-			temp |= 0xFFFF0000;
-		length2 = (float)temp * LENGTH_SCALE;
+        if (prussdrv_exec_program (1, "./stepper.bin")) {
+                fprintf (stderr, "prussdrv_exec_program(1, './stepper.bin') failed\n");
+                return 1;
+        }
 
-		pruDataMem1_int[0] = (int)(length1 * 100.0);
-		pruDataMem1_int[1] = (int)(length2 * 100.0);
+        pressHandle1 = i2c_open (1, 0x28);
+        pressHandle2 = i2c_open (2, 0x28);
 
-		printf ("%d, %d\n", (int)(length1 * 100.0), (int)(length2 * 100.0));
-	}
+        sock = inetClientInit ("192.168.1.102");
+
+        while (1) {
+                temp = pruDataMem0_int[0];
+                if (temp & (1 << 15))
+                        temp |= 0xFFFF0000;
+                length1 = (float)temp * LENGTH_SCALE;
+
+                temp = pruDataMem0_int[1];
+                if (temp & (1 << 15))
+                        temp |= 0xFFFF0000;
+                length2 = (float)temp * LENGTH_SCALE;
+
+                pruDataMem1_int[0] = (int)(length1 * 100.0);
+                pruDataMem1_int[1] = (int)(length2 * 100.0);
+
+                recv (sock, buffer, 255, 0);
+
+                printf ("%d, %d, %f\n", (int)(length1 * 100.0), (int)(length2 * 100.0), atof (buffer));
+        }
 
 /*
-	while (1) {
-		temp = pruDataMem0_int[0];
-		if (temp & (1 << 15))
-			temp |= 0xFFFF0000;
-		length1 = (float)temp * LENGTH_SCALE;
-		temp = pruDataMem0_int[1];
-		if (temp & (1 << 15))
-			temp |= 0xFFFF0000;
-		length2 = (float)temp * LENGTH_SCALE;
-		printf ("%f, %f\n", length1, length2);
-	}
+        while (1) {
+                temp = pruDataMem0_int[0];
+                if (temp & (1 << 15))
+                        temp |= 0xFFFF0000;
+                length1 = (float)temp * LENGTH_SCALE;
+                temp = pruDataMem0_int[1];
+                if (temp & (1 << 15))
+                        temp |= 0xFFFF0000;
+                length2 = (float)temp * LENGTH_SCALE;
+                printf ("%f, %f\n", length1, length2);
+        }
 */
 
 /*
-	while (1) {
-		nanosleep (&waitTime, NULL);
-		temp = i2cRead (pressHandle1);
-		pressure1 = (float)temp * PRESSURE_SCALE;
-		temp = i2cRead (pressHandle2);
-		pressure2 = (float)temp * PRESSURE_SCALE;
-		printf ("%f, %f\n", pressure1, pressure2);
-	}
+        while (1) {
+                nanosleep (&waitTime, NULL);
+                temp = i2cRead (pressHandle1);
+                pressure1 = (float)temp * PRESSURE_SCALE;
+                temp = i2cRead (pressHandle2);
+                pressure2 = (float)temp * PRESSURE_SCALE;
+                printf ("%f, %f\n", pressure1, pressure2);
+        }
 */
 
 shutdown:
 
-	pruTerminate ();
+        pruTerminate ();
 
-	return 0;
+        return 0;
 }
+
--- server/control.c
+++ server/control.c
@@ -8,6 +8,9 @@
 #include <fcntl.h>
 #include <linux/spi/spidev.h>
 
+#include <sys/socket.h>
+#include <arpa/inet.h>
+
 #define FORCE_SCALE 1.164153357e-4 //(0.5 * 3.3 / 128) / (2^23 - 1) / (2e-3 * 3.3) * 500
 
 static int spiBits = 8;
@@ -31,7 +34,7 @@ void spiInit (char *file, int *fd)
 
 float spiRead24 (int fd)
 {
-	uint8_t tx[3] = {1, 1, 1};
+	uint8_t tx[3] = {0, 0, 0};
 	uint8_t rx[3];
 	struct spi_ioc_transfer tr;
 
@@ -46,12 +49,47 @@ float spiRead24 (int fd)
         return rx[0] << 16 | rx[1] << 8 | rx[2];
 }
 
+int inetServerInit (void)
+{
+	int sock;
+	int returnSock;
+	struct sockaddr_in server;
+
+	if ((sock = socket (AF_INET, SOCK_STREAM, 0)) < 0) {
+		fprintf (stderr, "socket(AF_INET, SOCK_STREAM, 0) failed\n");
+		return -1;
+	}
+
+	server.sin_addr.s_addr = INADDR_ANY;
+	server.sin_family = AF_INET;
+	server.sin_port = htons (8888);
+
+	if (bind (sock, (struct sockaddr *)&server, sizeof(server)) < 0) {
+		fprintf (stderr, "bind() failed\n");
+		return -1;
+	}
+
+	listen (sock, 3);
+
+	if ((returnSock = accept (sock, NULL, NULL)) < 0) {
+		fprintf (stderr, "accept() failed\n");
+		return -1;
+	}
+
+	return returnSock;
+}
+
 int main (int argc, char **argv)
 {
 	int spiFile1;
 	int spiFile2;
+
+	int sock;
+	char buffer[255];
+
 	int temp;
 	float force;
+
 	struct timespec waitTime = {0, 100000000};
 
 	if (setjmp (buf))
@@ -62,13 +100,17 @@ int main (int argc, char **argv)
 	spiInit ("/dev/spidev1.0", &spiFile1);
 	spiInit ("/dev/spidev2.0", &spiFile2);
 
+	sock = inetServerInit ();
+
 	while (1) {
 		nanosleep (&waitTime, NULL);
 		temp = spiRead24 (spiFile2);
 		if (temp & (1 << 23))
 			temp |= 0xFF000000;
 		force = (float)temp * FORCE_SCALE;
-		printf ("%f\n", force);
+
+		sprintf (buffer, "%f", force);
+		send (sock, buffer, 255, 0);
 	}
 
 shutdown:
